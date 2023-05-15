ebcd146c70cdf056618c12f1c2e2cbb49888ade5
Marcel Meulemans <marcel@meulemans.org>
2023-05-16 09:13:02 +1200

    chore: add github action

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+}
+
+cd ${script_dir}
 VERSION=$(awk '{if (match($0,/^define.+ORDERABLE_ORG_VERSION/) && match($0,/[0-9]+\.[0-9]+\.[0-9]+/,m)) print m[0] }' orderable-org/orderable-org.php)
 rm -f orderable-org*.zip
-zip -rq orderable-org-${VERSION}.zip orderable-org/
+zip -r orderable-org-${VERSION}.zip orderable-org/
